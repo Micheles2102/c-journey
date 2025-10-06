04186a977300c36bbf379c1717671af463a96824
thisismichele <michelemessina948@gmail.com>
2025-10-06 16:56:22 +0200

    Mark Two:correct errors

--- manual-malloc/Personal_malloc/memory_pool.c
+++ manual-malloc/Personal_malloc/memory_pool.c
@@ -4,65 +4,95 @@ void* allocator(size_t size){
    if(size<=8){
         void* breakingpoint=NULL;
         if(!memory.bin_8){
-            memory.bin_8=allocate_memory_for_bin(4);
-            memory.bin_8->ptr_iniziale=allocate_memory(32);//voglio che crea 4 blocchi da 8
-             //mi setto un breaking point
-            breakingpoint=memory.bin_8->ptr_iniziale+31;
+            if(!(memory.bin_8=allocate_memory_for_bin(4))){
+                perror("Memory allocation bin error");
+                exit(-1);
+            }
+
+            if(!(memory.bin_8->ptr_iniziale=allocate_memory(4*8))){
+                //voglio che crea 4 blocchi da 8
+                perror("Memory allocation blocks error");
+                exit(-2);
+            }
+            
+            //mi setto un breaking point
+            //siccome sto lavorando su bye con void* caso (void*)((char*) in modo tale da potere modificare spostandomi e lo ricasto a void*
+            breakingpoint=(void*)((char*)memory.bin_8->ptr_iniziale+31);
 
             for(int i=0;i<4;i++){
                 memory.bin_8[i].is_free=1;
-                memory.bin_8[i].ptr_finale=memory.bin_8[i].ptr_iniziale+7;
-                memory.bin_8[i].ptr_next=&memory.bin_8[i]+1;
-                if(memory.bin_8[i].ptr_finale+1==breakingpoint){
+                memory.bin_8[i].ptr_finale=(void*)((char*)memory.bin_8[i].ptr_iniziale+7);
+                //quando arriva ad avere i=4 non farà l'ultimo codice a riga 20 perchp si interrompe qui
+                if((void*)((char*)memory.bin_8[i].ptr_finale+1)==breakingpoint){
+                    memory.bin_8[i].ptr_next=NULL;
                     break;
                 }
-                memory.bin_8[i].ptr_iniziale=memory.bin_8[i].ptr_finale+1;
+                memory.bin_8[i].ptr_next=&memory.bin_8[i+1];
+                memory.bin_8[i+1].ptr_iniziale=(void*)((char*)memory.bin_8[i].ptr_iniziale+8);
             }
         }
 
-        //fino a quando vede che non ci sono blochci disponibili
-        while(memory.bin_8->is_free==0){
-            //controlla se il successivo è il breaking point
-            if(memory.bin_8->ptr_next != breakingpoint)
-                //se non lo è vai al successivo
-                memory.bin_8=memory.bin_8->ptr_next;
-            //se dovesse esserlo
-            perror("Impossibile creare nuova memoria,tutti i blocchi sono occupati; liberare qualcosa");
-            return NULL;
+         //fino a quando vede che non ci sono blochci disponibili
+        header_t* tmp=memory.bin_8;
+        while(tmp->is_free==0){
+            //controlla se il successivo è il breaking point(dunque NULL)
+            if(!tmp->ptr_next){
+                //se dovesse esserlo
+                perror("Impossibile creare nuova memoria,tutti i blocchi sono occupati; liberare qualcosa");
+                return NULL;
+            }
+            //se non lo è vai al successivo
+            tmp=tmp->ptr_next;
+            
         }
-        memory.bin_8->is_free=0;
-        return memory.bin_8->ptr_iniziale;
+        tmp->is_free=0;
+        return tmp->ptr_iniziale;
    }
-   if(size<32 && size>8){
+   if(size<=16 && size>8){
         void* breakingpoint=NULL;
         if(!memory.bin_16){
-            memory.bin_16=allocate_memory_for_bin(4);
-            memory.bin_16->ptr_iniziale=allocate_memory(96);//voglio che crea 3 blocchi da 32
-             //mi setto un breaking point
-            breakingpoint=memory.bin_16->ptr_iniziale+95;
+            if(!(memory.bin_16=allocate_memory_for_bin(3))){
+                perror("Memory allocation bin error");
+                exit(-1);
+            }
 
-            for(int i=0;i<4;i++){
+            if(!(memory.bin_16->ptr_iniziale=allocate_memory(3 * 16))){
+                // voglio che crei 3 blocchi da 16 byte ciascuno
+
+                perror("Memory allocation blocks error");
+                exit(-2);
+            }
+            //mi setto un breaking point
+            breakingpoint=(void*)((char*)memory.bin_16->ptr_iniziale+47);
+
+            for(int i=0;i<3;i++){
                 memory.bin_16[i].is_free=1;
-                memory.bin_16[i].ptr_finale=memory.bin_16[i].ptr_iniziale+7;
-                memory.bin_16[i].ptr_next=&memory.bin_16[i]+1;
-                if(memory.bin_16[i].ptr_finale+1==breakingpoint){
+                memory.bin_16[i].ptr_finale=(void*)((char*)memory.bin_16[i].ptr_iniziale+15);
+                //quando arriva ad avere i=4 non farà l'ultimo codice a riga 20 perchp si interrompe qui
+                if((void*)((char*)memory.bin_16[i].ptr_finale+1)==breakingpoint){
+                    memory.bin_16[i].ptr_next=NULL;
                     break;
                 }
-                memory.bin_16[i].ptr_iniziale=memory.bin_16[i].ptr_finale+1;
+                memory.bin_16[i].ptr_next=&memory.bin_16[i+1];
+                memory.bin_16[i+1].ptr_iniziale=(void*)((char*)memory.bin_16[i].ptr_iniziale+16);
             }
         }
 
         //fino a quando vede che non ci sono blochci disponibili
-        while(memory.bin_16->is_free==0){
-            //controlla se il successivo è il breaking point
-            if(memory.bin_16->ptr_next != breakingpoint)
-                //se non lo è vai al successivo
-                memory.bin_16=memory.bin_16->ptr_next;
-            //se dovesse esserlo
-            perror("Impossibile creare nuova memoria,tutti i blocchi sono occupati; liberare qualcosa\n");
-            return NULL;
+        header_t* tmp=memory.bin_16;
+        while(tmp->is_free==0){
+            //controlla se il successivo è il breaking point(dunque NULL)
+            if(!tmp->ptr_next){
+                //se dovesse esserlo
+                perror("Impossibile creare nuova memoria,tutti i blocchi sono occupati; liberare qualcosa");
+                return NULL;
+            }
+            //se non lo è vai al successivo
+            tmp=tmp->ptr_next;
+            
         }
-        memory.bin_16->is_free=0;
-        return memory.bin_16->ptr_iniziale;
+        tmp->is_free=0;
+        return tmp->ptr_iniziale;
    }
+   return NULL; 
 }
