3da93804640657a59da1902d82abece33c2575be
thisismichele <michelemessina948@gmail.com>
2025-10-08 18:02:37 +0200

    add data+memory_pool allocation

--- manual-malloc/Personal_malloc/allocator.c
+++ manual-malloc/Personal_malloc/allocator.c
@@ -1,6 +1,18 @@
 #include "allocator.h"
+memory_pool* memory;
+int blocchi_8=4;
+int blocchi_16=3;
+int blocchi_32=2;
 
-
+int allocate_memory_pool(){
+    memory=mmap(NULL,sizeof(memory_pool),PROT_READ | PROT_WRITE,MAP_PRIVATE | MAP_ANONYMOUS,-1,0);
+    if(memory==MAP_FAILED){
+        perror("Memory pool allocation error");
+        return -1;
+    }
+    INIZIO=0;
+    return 1;
+}
 void* allocate_memory(size_t size){
     void* ptr=mmap(NULL,size,PROT_READ | PROT_WRITE,MAP_PRIVATE | MAP_ANONYMOUS,-1,0);
     if(ptr==MAP_FAILED){
@@ -11,7 +23,7 @@ void* allocate_memory(size_t size){
 }
 
 void* allocate_memory_for_bin(size_t size){ //sono uno contiiguo all'altro (come indirizzo virtuale)
-    void* ptr=mmap(NULL,size*sizeof(header_t),PROT_READ|PROT_WRITE,MAP_PRIVATE | MAP_ANONYMOUS,-1,0);
+    void* ptr=mmap(NULL,size,PROT_READ|PROT_WRITE,MAP_PRIVATE | MAP_ANONYMOUS,-1,0);
     if(ptr==MAP_FAILED){
         perror("Memory allocation error\n");
         return NULL;
