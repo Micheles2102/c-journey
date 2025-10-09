23fa3d7328c95f3d105dffe12cefb9ebe4881224
thisismichele <michelemessina948@gmail.com>
2025-10-09 06:27:18 +0200

    working on free memory

--- manual-malloc/Personal_malloc/deallocator.c
+++ manual-malloc/Personal_malloc/deallocator.c
@@ -1,5 +1,5 @@
 #include "allocator.h"
-
+//Some functions will be used instead of exit() to free allocated memory
 int deallocate_memory(void* ptr,size_t size){
     int tmp=munmap(ptr,size);
     if(tmp==-1){
