c667dc177163ca83ad5a6c099bd95b3b0c038ddf
thisismichele <michelemessina948@gmail.com>
2025-10-10 18:41:25 +0200

    add test:Check done it works

--- manual-malloc/Personal_malloc/main.c
+++ manual-malloc/Personal_malloc/main.c
@@ -3,14 +3,22 @@
 
 int main(){
 
-    int *data=allocator(sizeof(int*)*5);
+    int *data=allocator(sizeof(int)*5);
 
-    for(int i=0;i<8;i++){
+    for(int i=0;i<5;i++){
         data[i]=i;
         printf("Il valore di data e' : %d\n",data[i]);
     }
+    printf("data points to: %p\n", (void*)data);
 
-    deallocator(data);
+    int test=1;
+    if((test=deallocator(data,sizeof(int)*5))!=1){
+        perror("Error during free");
+        return -1;
+    }
+    printf("il valore di test è %d",test);
+    data = NULL;
 
+    printf("è andato tutto bene");
     return 0;
 }
