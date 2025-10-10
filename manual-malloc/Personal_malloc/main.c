3805523f501f4386c6191adde8d825a604add137
thisismichele <michelemessina948@gmail.com>
2025-10-10 18:57:31 +0200

    Second Check Done: SUCCESS

--- manual-malloc/Personal_malloc/main.c
+++ manual-malloc/Personal_malloc/main.c
@@ -1,24 +1,28 @@
 #include "allocator.h"
 #include <stdio.h>
 
+/* //CHECK DONE
+gcc  allocator.c memory_pool.c allocator.h deallocator.c main.c -o test
+strace -e mmap,munmap ./test
+valgrind --leak-check=full ./test */
+
 int main(){
+    int size=2;
+    int *data=allocator(sizeof(int)*size);
 
-    int *data=allocator(sizeof(int)*5);
-
-    for(int i=0;i<5;i++){
+    for(int i=0;i<size;i++){
         data[i]=i;
-        printf("Il valore di data e' : %d\n",data[i]);
+        printf("Data value is' : %d\n",data[i]);
     }
     printf("data points to: %p\n", (void*)data);
 
     int test=1;
-    if((test=deallocator(data,sizeof(int)*5))!=1){
+    if((test=deallocator(data,sizeof(int)*size))!=1){
         perror("Error during free");
         return -1;
     }
-    printf("il valore di test è %d",test);
+    printf("Test: %d",test);
     data = NULL;
 
-    printf("è andato tutto bene");
     return 0;
 }
